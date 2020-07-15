4f7a53c89f18b43cde1f1622d75eb3292891e8e6
Paule <popomartel@live.com>
2020-07-15 16:31:20 -0400

    debut calcul phase 1 pas fini

--- TP2.cpp
+++ TP2.cpp
@@ -25,6 +25,11 @@ using namespace std;
 
 
 vector< Histoire * > *
+
+
+
+
+
 lireDocuments( string a_nomFichier )
 {
     vector< Histoire * > * histoires = new vector< Histoire * >();
@@ -59,12 +64,23 @@ lireDocuments( string a_nomFichier )
     return histoires;
 }
 
+//declaration de fonctions
+//void creerArbresAvlHistoire(&vector< Histoire * > * histoires);
+void creerArbresAvlHistoire(vector< Histoire * > * const & histoires,
+ vector<ArbreAVL<string>> & arbresAvls);
+
 
 int main() {
-    // gardez la ligne suivante, elle lit le corpus et le place dans la structure de base.
-    // Vous avez donc un vecteur d'histoire qui contient l'information sur les histoires,
-    // les Phrases et les mots qu'elles contiennent.
+
+    //contient les histoires
     vector< Histoire * > * histoires = lireDocuments( string( "listeDocument.xml" ) );
+
+    //vecteur qui va contenir les arbres AVL de chaque histoire
+    vector<ArbreAVL<string>> arbresAvls;
+
+    creerArbresAvlHistoire(histoires,arbresAvls);
+
+
 
 
     // Pour votre projet, enlevez le code qui suit et remplacer le par votre code.
@@ -74,7 +90,7 @@ int main() {
     for( Histoire * histoire : * histoires )
     {
         // Les histoires ont une variable de classe 'titre'.
-        cout << histoire->titre() << endl;
+        //cout << histoire->titre() << endl;
 
         // Parcourir les Phrases qui compose une histoire à l'aide de l'iterateur des Histoires.
         for( Phrase p : * histoire )
@@ -82,12 +98,49 @@ int main() {
             // p.begin() va chercher le premier mot de la Phrase p.  c'est aussi un iterateur et il peut
             // s'utiliser avec les for augmentés.
             // ici, nous affichons seulement le premier mot de la Phrase.
-            cout << *( p.begin() ) << endl;
+            //cout << *( p.begin() ) << endl;
         }
 
-        cout << endl;
+        //cout << endl;
     }
 
     return 0;
 }
+
+/**
+Parcourt chaque histoire du vecteur histoire et creer un
+arbre AVL pour categorisant chaque mot de l'histoire ou
+la clef est le mot en string et la definition est le 
+nombre d'occurence du mot dans l'histoire
+@param &vector< Histoire * > * histoires, le vecteur
+contenant les histoire
+@param vector<ArbreAVL<string>> & arbresAvls le vecteur
+qui stocke les arbre avl de chaque mot de l'histoire
+**/
+
+//vector<Pile<string,int>> arbresAvls
+
+//TODO : EST CE QU'IL FAUT AVOIR DES MOTS a 0?
+void creerArbresAvlHistoire(vector< Histoire * > * const & histoires,
+ vector<ArbreAVL<string>> & arbresAvls ){
+    ArbreAVL<string> arbre; //comment mettre nombre d'occurence en plus?
+    for( Histoire * histoire : * histoires ){
+        //cout << histoire->titre() << endl;
+        for( Phrase p : * histoire ){
+            //cout << *(p.end()) << endl;
+            // for(int i = 0 ; i < p.end() ; i++){
+            //     //cout << *( p.begin() + i ) << endl;
+
+            // }
+            //cout << *( p.begin() ) << endl
+
+        }
+
+    }
+
+
+}
+
+
+
 
